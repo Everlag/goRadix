9819970e8ed356b6b7f55bb6867d4131739ed127
Kyle Boutette <contact@perfectlag.me>
2015-07-27 11:25:29 -0600

    Tried caching positions, not worthwhile.
    Small performance penalty incurred when caching the result of computing
    a position.

--- radix.go
+++ radix.go
@@ -8,23 +8,25 @@ func Ints(a []int) {
 	// Bucket on 0 or 1
 	bucketLen:= 2
 	buckets:= make([]int, bucketLen)
+	positions:= make([]int, len(a))
 
 	// Assume 64 bit integers. Can do early exit later.
-	var pos int = 0
 	var i uint = 0
 	var n int
+	var pos int = 0
 	var pow2 int
 	for ; i < 64; i++ {
 	
 		pow2 = 1 << i
 
 		// Populate buckets
-		for _, n:= range a{
+		for p, n:= range a{
 
 			// Find if the bit is set then shift
 			// right to avoid a conditional
 			pos = (n & pow2) >> i
-			buckets[pos]+= 1
+			positions[p] = pos
+			buckets[positions[p] ]+= 1
 
 		}
 
@@ -35,7 +37,7 @@ func Ints(a []int) {
 		for p:= (len(a) - 1); p >= 0; p-- {
 			
 			n = a[p]
-			pos = (n & pow2) >> i
+			pos = positions[p]
 			buckets[pos]--
 			scratch[buckets[pos]] = n
 
